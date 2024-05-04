e480dc0669c3188169a051de2245c6584cb769f0
kevinnadr <kevin19305.ib@gmail.com>
2024-05-04 13:41:57 +0700

    step 5

--- linklist.cpp
+++ linklist.cpp
@@ -32,4 +32,20 @@ void addNode()
 		{
 			START->prev = newNode; // step 4: make the first node to the new node
 		}
-		
+		newNode->prev = NULL; // step 5: make the new node point to NULL
+		START = newNode; // step 6: make the new node the first node
+	}
+	else
+	{
+		// insert the new node in the middle or at the end
+		Node* current = START; // step 1.a: start from the first node
+		Node* previous = NULL; // step 1.b: previous node is NULL initially
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{                             // step 1.c: traverse the list to find the correct position
+			previous = current;       // step 1.d: move the previous to the current node
+			current = current->next;  // step 1.e: move the current to the next node
+		}
+
+		newNode->next = current;   // step 4: make the next field of the new node point to current
+		newNode->prev = previous;  // step 5: mkae the previous field of the node point the previous
